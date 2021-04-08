0dab437234894b11df10d48c9e820f997cf8b695
טמיר מץ <tamirmatz@h-MacBook-Pro-sl-tmyr.local>
2021-04-08 16:29:55 +0300

    change

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ function contactMail() {
     $('.body-input').val();
     $('.subject-input').val();
     window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=tamirmatz@gmail.com&su=${$('.subject-input').val()}&body=${$('.body-input').val()}`);
+    $('.body-input').val() = '';
+    $('.subject-input').val() = '';
 }
 
 function initPage() {
